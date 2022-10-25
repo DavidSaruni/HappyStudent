2eb3d23d8f4eb2c2cf71528dd560583e93f600fa
SoftCysec <softengdedan@gmail.com>
2022-10-25 15:54:33 +0300

    Super Admin Code Modified

--- base/models.py
+++ base/models.py
@@ -1,15 +1,16 @@
+from enum import unique
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 
 class User(AbstractUser):
+    username = models.CharField(max_length=200, unique=True, null=True)
     name = models.CharField(max_length=200, null=True)
-    email = models.EmailField(unique=True, null=True)
     bio = models.TextField(null=True)
 
     avatar = models.ImageField(null=True, default="avatar.svg")
-
-    USERNAME_FIELD = 'email'
+ 
+    USERNAME_FIELD = 'username'
     REQUIRED_FIELDS = []
 
 
