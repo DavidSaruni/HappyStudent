3db304f0769adeef4ca7aab6ad10a67529895376
flairsec <samsimiyu268@gmail.com>
2022-10-29 12:35:40 +0300

    star rating module

--- base/admin.py
+++ base/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from .models import Message, Room, Topic, User
+from .models import Message, Room, Topic, User, Rating
 
 # Register your models here.
 
@@ -9,3 +9,4 @@ admin.site.register(User)
 admin.site.register(Room)
 admin.site.register(Topic)
 admin.site.register(Message)
+admin.site.register(Rating)
